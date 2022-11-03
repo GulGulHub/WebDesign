c5f452f551d550c17f93b70ce1a5c8e83afe51a6
GulGulHub <guelden.ediger@gmx.net>
2022-11-03 18:28:46 +0100

    inserted bootstrap in new folder, working on getting image in header next to heading - then in old folder project - i am working on creating a slider with 3 images

---
+++ old_design/javascript/projekte.js
@@ -0,0 +1,34 @@
+arrowLeft = document.querySelector('#arrow-left')
+arrowRight = document.querySelector('#arrow-right')
+slideArray = document.querySelectorAll('.array')
+
+takeSlide= document.getElementsByClassName('.carousel2')
+
+
+
+
+makeVis=takeSlide.style.visibility='hidden'
+
+console.log(slideArray)
+
+
+
+
+/*
+
+arrowLeft.addEventListener('click', displayNext)
+
+function displayNext (indexOfArrayDiv){
+
+    for (let  i=0; i<slideArray.length; i++){
+      make next div visible, make last invisible
+    }
+    
+    add to the page
+} 
+
+
+
+
+
+*/ 
