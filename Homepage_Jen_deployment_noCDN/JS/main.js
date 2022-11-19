f05382e4183dee7d56f0639adaf31d02839cf010
GulGulHub <guelden.ediger@gmx.net>
2022-11-19 20:25:43 +0100

    finished carousel, changed into no cdn, added links and click on JS

---
+++ Homepage_Jen_deployment_noCDN/JS/main.js
@@ -0,0 +1,61 @@
+img = document.querySelector(".click-event1");
+img.addEventListener('click', () => {
+    // open a new tab
+    const tab = window.open('https://www.merve.de/index.php/book/show/504', '_blank')
+})
+
+
+
+img2 = document.querySelector(".click-event2");
+img2.addEventListener('click', () => {
+    // open a new tab
+    const tab = window.open('https://www.merve.de/index.php/book/show/472', '_blank')
+})
+
+
+
+img3 = document.querySelector(".click-event3");
+img.addEventListener('click', () => {
+    // open a new tab
+    const tab = window.open('https://unrast-verlag.de/index.php/gesamtprogramm/allgemeines-programm/antirassismus/anders-europaeisch-detail', '_blank')
+})
+
+
+
+img4 = document.querySelector(".click-event4");
+img4.addEventListener('click', () => {
+    // open a new tab
+    const tab = window.open('https://www.transcript-verlag.de/978-3-8376-5054-9/frische-fruechte-kaputte-koerper/?number=978-3-8394-5054-3', '_blank')
+})
+
+
+
+img5 = document.querySelector(".click-event5");
+img5.addEventListener('click', () => {
+    // open a new tab
+    const tab = window.open('https://unrast-verlag.de/index.php/neuerscheinungen/queer-detail', '_blank')
+})
+
+
+
+img6 = document.querySelector(".click-event6");
+img6.addEventListener('click', () => {
+    // open a new tab
+    const tab = window.open('https://wortenundmeer.net/product/sherri-mitchell-aktivismus-heisst-verbindung/', '_blank')
+})
+
+img7 = document.querySelector(".click-event7");
+img7.addEventListener('click', () => {
+    // open a new tab
+    const tab = window.open('https://orlanda.de/book/nur-ja-heisst-ja', '_blank')
+})
+
+
+
+img8 = document.querySelector(".click-event8");
+img8.addEventListener('click', () => {
+    // open a new tab
+    const tab = window.open('https://www.bbooks.de/verlag/die-schwarzen-jakobiner', '_blank')
+})
+
+
